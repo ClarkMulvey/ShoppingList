8386f2b9b29b41a70be00940a16974f2600b611c
Clark Mulvey <clarkgmulvey2@gmail.com>
2021-02-23 00:52:35 -0700

    Added loadList() and saveList() methods - the data for the default list will now persist through app sessions

--- app/src/main/java/com/example/shoppinglist/EditDefaultListActivity.java
+++ app/src/main/java/com/example/shoppinglist/EditDefaultListActivity.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 public class EditDefaultListActivity extends AppCompatActivity {
 
-    ArrayList<ShoppingListItem> defaultList;
+    ShoppingListDefault defaultList;
     EditText itemName;
     EditText itemQuantity;
     ListView listView;
@@ -30,23 +30,31 @@ public class EditDefaultListActivity extends AppCompatActivity {
         this.itemQuantity = (EditText) findViewById(R.id.ItemQuantity);
         this.listView = (ListView) findViewById(R.id.listView);
 
-        defaultList = loadList();
-        if (defaultList == null) {
-            defaultList = new ArrayList<>();
-        }
+        this.defaultList = loadList("default");
+        displayDefaultList();
     }
 
-    public ArrayList<ShoppingListItem> loadList() {
-        return null;
+    public ShoppingListDefault loadList(String listName) {
+
+        Gson gson = new Gson();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.shoppinglist", MODE_PRIVATE);
+
+        String defaultListString = sharedPreferences.getString(listName, "");
+        ShoppingListDefault loadedList = gson.fromJson(defaultListString, ShoppingListDefault.class);
+
+        if (loadedList != null) {
+            return loadedList;
+        }
+
+        return new ShoppingListDefault(listName);
     }
 
     public void displayDefaultList() {
         ArrayList<String> listPopulator = new ArrayList<>();
-
-        for (int i = 0, max = this.defaultList.size(); i < max; i++) {
-            listPopulator.add(this.defaultList.get(i).getQuantity() + " " + this.defaultList.get(i).getName());
+        for (int i = 0, max = this.defaultList.getItems().size(); i < max; i++) {
+            listPopulator.add(this.defaultList.getItems().get(i).getQuantity() + " " + this.defaultList.getItems().get(i).getName());
         }
-
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listPopulator);
 
         this.listView.setAdapter(arrayAdapter);
@@ -58,9 +66,27 @@ public class EditDefaultListActivity extends AppCompatActivity {
     }
 
     public void addItemToDefaultList(View view) {
-        this.defaultList.add(new ShoppingListItem(this.itemName.getText().toString(), Integer.parseInt(this.itemQuantity.getText().toString())));
+        this.defaultList.addItem(new ShoppingListItem(this.itemName.getText().toString(), Integer.parseInt(this.itemQuantity.getText().toString())));
         Toast.makeText(this, this.itemName.getText().toString() + " has been added to the list" , Toast.LENGTH_LONG).show();
         clearFields();
         displayDefaultList();
+        saveList();
+    }
+
+    public void saveList() {
+
+        Gson gson = new Gson();
+
+        String listStorage = gson.toJson(this.defaultList);
+
+        // Storing data into SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.shoppinglist", MODE_PRIVATE);
+
+        // Creating an Editor object to edit(write to the file)
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+        myEdit.putString(defaultList.getName(), listStorage);
+
+        myEdit.apply();
     }
 }
