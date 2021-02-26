3001f978e53e5da8312c6ffbf6604a43d478cf78
meflav <tru19006@byui.edu>
2021-02-25 21:15:11 -0500

    Add buttons link to activities

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.shoppinglist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.firebase.database.FirebaseDatabase;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
 
     public void clickViewEditDefaultList(View view) {
         Intent intent = new Intent(this, EditDefaultListActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickStartShopping(View view) {
+        Intent intent = new Intent(this, StartShoppingActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickViewUpcomingList(View view) {
+        Intent intent = new Intent(this, ViewUpcomingListsActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickCreateUpcomingTripList(View view) {
+        Intent intent = new Intent(this, CreateUpcomingTripListActivity.class);
         startActivity(intent);
     }
 }
